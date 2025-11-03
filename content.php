be3b830b828fec454a4d07b874c8ff142a31c315
Diyar Azhir <diyarazhir@outlook.com>
2025-11-03 11:25:16 +0330

    Update content.php

--- content.php
+++ content.php
@@ -1,1 +1,17 @@
 
+
+	<div class="mainframe">
+	
+		<!--p id="input" contenteditable="true" onkeydown="fncr()" onkeyup="fncr()">d</p-->
+		
+		<input type="text" id="input" onkeydown="fncr()" onkeyup="fncr()">
+		
+		<p id="output">m</p>
+		
+		<p id="pointer" onclick="clicw()">_</p>
+		
+		<p id="ex"></p>
+	
+	</div>
+
+
